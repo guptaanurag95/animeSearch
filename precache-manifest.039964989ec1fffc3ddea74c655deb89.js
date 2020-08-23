72fa352e80daae56df7bd5ed4f463859225c9a0a
guptaanurag95 <guptaanurag5395@gmail.com>
2020-08-23 22:03:19 +0530

    Updates

---
+++ precache-manifest.039964989ec1fffc3ddea74c655deb89.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1434197c89825ee4286274c354029766",
+    "url": "/animeSearch/index.html"
+  },
+  {
+    "revision": "32414703a45b9bf29ddd",
+    "url": "/animeSearch/static/js/2.95cd62c7.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/animeSearch/static/js/2.95cd62c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "208ac501a4e418048911",
+    "url": "/animeSearch/static/js/main.85a258d9.chunk.js"
+  },
+  {
+    "revision": "c0a5dd29b12867601ae9",
+    "url": "/animeSearch/static/js/runtime-main.94b4d285.js"
+  }
+]);
